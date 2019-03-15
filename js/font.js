01934edd9f24a3c29f08e6d777d654c10a2d27d6
Ruben Garcia <rubengcdev@gmail.com>
2019-03-15 16:58:29 +0100

    1.0.0
    Initial release

---
+++ js/font.js
@@ -0,0 +1,19 @@
+(function( $ ) {
+
+    $( '.cmb-type-font select' ).each(function() {
+
+        $(this).higooglefonts({
+            theme: 'default cmb-font-select2',
+            selectedCallback:function(e){
+                console.log(e);
+            },
+            loadedCallback:function(font){
+                console.log(font);
+                /*/////// This is where you should apply font.///////
+                /////////////////////////////////////////////////////*/
+                $("#paragraph").css("font-family", font); // Change the font-family of the #paragraph
+            }
+        });
+    });
+
+})(jQuery);
